5398a972c43b584e57edab83fff40cc77a3185d1
adamklff <khlifiadam1@gmail.com>
2026-01-19 22:13:15 +0100

    Delete gestion_de_soutenance directory

--- gestion_de_soutenance/includes/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <p>&copy; 2026 - Gestion Soutenances</p>
-</footer>
-</body>
-</html>
--- gestion_de_soutenance/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'includes/config.php';
-$db = getDB();
-echo "Connection OK!<br>";
-echo "Current time: " . date('Y-m-d H:i:s');
-?>
